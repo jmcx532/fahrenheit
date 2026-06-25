e194efe8387cf6e56edbae510c1acfd0aaf6f07e
jmcx532 <jmcx532@hotmail.com>
2026-06-25 17:59:00 +0100

    Update Job.cs

--- src/core/ffx2/Job.cs
+++ src/core/ffx2/Job.cs
@@ -1,141 +1,123 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+﻿// SPDX-License-Identifier: LGPL-3.0-or-later
 //
 // This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
 // It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
 
-// ffx_ps2/ffx2/master/jppc/battle/kernel/job.h
-// Switch release of FFX/X-2 HD
-
 namespace Fahrenheit.FFX2;
 
-public partial struct Job
-{
-    [NativeTypeName("unsigned int")]
-    public uint name;
+[StructLayout(LayoutKind.Explicit, Size = 0xE4)]
+public struct Job {
+    [FieldOffset(0x00)] public ushort            name_string_offset;
+    // [FieldOffset(0x02)] public ushort         name_unk; 
+    [FieldOffset(0x04)] public ushort            help_string_offset;
+    // [FieldOffset(0x06)] public ushort         help_unk;
+    [FieldOffset(0x08)] public byte              user;
+    [FieldOffset(0x09)] public byte              data;
+    [FieldOffset(0x0A)] public byte              seq;
+    [FieldOffset(0x0B)] public byte              icon;
+    [FieldOffset(0x0C)] public ushort            berserk_action;
 
-    [NativeTypeName("unsigned int")]
-    public uint help;
+    [FieldOffset(0xE)]  public StatGrowth3       hp_up;
+    [FieldOffset(0x11)] public StatGrowth3       mp_up;
 
-    [NativeTypeName("unsigned char")]
-    public byte user;
+    [FieldOffset(0x14)] public StatGrowth5       str_up;
+    [FieldOffset(0x19)] public StatGrowth5       def_up;
+    [FieldOffset(0x1E)] public StatGrowth5       mag_up;
+    [FieldOffset(0x23)] public StatGrowth5       mdef_up;
+    [FieldOffset(0x28)] public StatGrowth5       agl_up;
+    [FieldOffset(0x2D)] public StatGrowth5       eva_up;
+    [FieldOffset(0x32)] public StatGrowth5       acc_up;
+    [FieldOffset(0x37)] public StatGrowth5       luck_up;
 
-    [NativeTypeName("unsigned char")]
-    public byte data;
+    [FieldOffset(0x3c)] public InlineArray16<JobAbility>      dressphere_abilities;
 
-    [NativeTypeName("unsigned char")]
-    public byte seq;
+    [FieldOffset(0x7c)] public JobWeapon         yuna_weapons;
+    [FieldOffset(0x8c)] public JobWeapon         rikku_weapons;
+    [FieldOffset(0x9c)] public JobWeapon         paine_weapons;
 
-    [NativeTypeName("unsigned char")]
-    public byte icon;
+    [FieldOffset(0xAC)] public JobCreatureData   creature_data;
 
-    [NativeTypeName("unsigned short")]
-    public ushort basaku;
-
-    [NativeTypeName("unsigned char[3]")]
-    public _hp_up_e__FixedBuffer hp_up;
-
-    [NativeTypeName("unsigned char[3]")]
-    public _mp_up_e__FixedBuffer mp_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _str_up_e__FixedBuffer str_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _vit_up_e__FixedBuffer vit_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _mag_up_e__FixedBuffer mag_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _spirit_up_e__FixedBuffer spirit_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _dex_up_e__FixedBuffer dex_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _avoid_up_e__FixedBuffer avoid_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _hit_up_e__FixedBuffer hit_up;
-
-    [NativeTypeName("unsigned char[4]")]
-    public _luck_up_e__FixedBuffer luck_up;
-
-    [NativeTypeName("unsigned short[32]")]
-    public _ability_e__FixedBuffer ability;
-
-    [NativeTypeName("short[3][8]")]
-    public _weapon_e__FixedBuffer weapon;
-
-    [InlineArray(3)]
-    public partial struct _hp_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(3)]
-    public partial struct _mp_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _str_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _vit_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _mag_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _spirit_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _dex_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _avoid_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _hit_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(4)]
-    public partial struct _luck_up_e__FixedBuffer
-    {
-        public byte e0;
-    }
-
-    [InlineArray(32)]
-    public partial struct _ability_e__FixedBuffer
-    {
-        public ushort e0;
-    }
-
-    [InlineArray(3 * 8)]
-    public partial struct _weapon_e__FixedBuffer
-    {
-        public short e0_0;
-    }
 }
+
+// stat growth
+[InlineArray(3)]
+public struct StatGrowth3 {
+    public byte e0;
+
+    public void set(byte a, byte b, byte c) {
+        this[0] = a;
+        this[1] = b;
+        this[2] = c;
+
+    }
+
+}
+
+[InlineArray(5)]
+public struct StatGrowth5 {
+    public byte e0;
+
+    public void set(byte a, byte b, byte c, byte d, byte e) {
+        this[0] = a;
+        this[1] = b;
+        this[2] = c;
+        this[3] = d;
+        this[4] = e;
+
+    }
+
+}
+
+//dressphere abilities
+[StructLayout(LayoutKind.Explicit, Size = 0x4)]
+public struct JobAbility{
+    [FieldOffset(0x00)] public ushort requirement;
+    [FieldOffset(0x02)] public ushort ability;
+}
+
+// weapons
+[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+public struct JobWeapon {
+    [FieldOffset(0x00)] public JobWeaponData weapon_1;
+    [FieldOffset(0x04)] public JobWeaponData weapon_2;
+    [FieldOffset(0x08)] public JobWeaponData weapon_3;
+    [FieldOffset(0x0C)] public JobWeaponData weapon_4;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x4)]
+public struct JobWeaponData {
+    [FieldOffset(0x00)] public  ushort weapon_model;
+    [FieldOffset(0x02)] public  ushort weapon_position;
+}
+
+// creature data - Int/Remaster
+[StructLayout(LayoutKind.Explicit, Size = 0x38)]
+public struct JobCreatureData{
+    [FieldOffset(0x00)] public  ushort          cre_help_string_offset;
+    //[FieldOffset(0x02)] public  ushort          cre_help_unknown;
+
+    [FieldOffset(0x04)] public  ushort          ability_prerequisite;
+    [FieldOffset(0x06)] public  ushort          ability;
+    [FieldOffset(0x08)] public  ushort          auto_ability_prerequisite;
+    [FieldOffset(0x0A)] public  ushort          auto_ability;
+    //[FieldOffset(0x0C)] public  byte[16]        unk_bytes;
+    [FieldOffset(0x1C)] public StatChanges        stat_changes; // buffs/debuffs creature stats
+    //[FieldOffset(0x26)] public  byte[18]        unk_bytes2;
+}
+
+// generic struct, also used in a_ability for example
+[StructLayout(LayoutKind.Explicit, Size = 0xA)]
+public struct StatChanges {
+    [FieldOffset(0x00)] public  sbyte hp;
+    [FieldOffset(0x01)] public  sbyte mp;
+    [FieldOffset(0x02)] public  sbyte str;
+    [FieldOffset(0x03)] public  sbyte def;
+    [FieldOffset(0x04)] public  sbyte mag;
+    [FieldOffset(0x05)] public  sbyte mdef;
+    [FieldOffset(0x06)] public  sbyte agility;
+    [FieldOffset(0x07)] public  sbyte accuracy;
+    [FieldOffset(0x08)] public  sbyte evasion;
+    [FieldOffset(0x09)] public  sbyte luck;
+}
+
+
